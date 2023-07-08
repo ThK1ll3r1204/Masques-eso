cba49df4ac4bddaed09dc9818926f1c49d56544c
Luis Gonzales <SteveLuis4555@gmail.com>
2023-07-08 15:40:25 -0500

    flechas de interacción en todos los NPC's, script a medias de interacción con los Npc's

--- Assets/NPC'S/NPC Interaction.cs
+++ Assets/NPC'S/NPC Interaction.cs
@@ -1,19 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NPCInteraction : MonoBehaviour
 {
-    public class Interaction : MonoBehaviour
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField, TextArea(4, 6)] private string[] dialogos;
+    [SerializeField] private TMP_Text dialogueText;
+    Interaction interaction;
+
+    private float tipeo = 0.05f;
+
+    private bool DialogueStart;
+    private int lineIndex;
+   void Awake()
     {
-        public GameObject interactionSignPrefab;
-        Interaction sign;
-
-        private void Awake()
+        interaction = transform.Find("wantInteract").GetComponent<Interaction>();
+    }
+    private void Update()
+    {
+        if(interaction.wantInteract == true)
         {
-            GameObject interactionSignObject = Instantiate(interactionSignPrefab, transform);
-            sign = interactionSignObject.GetComponent<Interaction>();
+            if(!DialogueStart)
+            {
+                StartDialogue();
+            }
         }
+    }
 
+    private void StartDialogue()
+    {
+        DialogueStart = true;
+        dialoguePanel.SetActive(true);
+        lineIndex = 0;
+        StartCoroutine(ShowLine());
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+
+        foreach(char ch in dialogos[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSeconds(tipeo);
+        }
     }
 }
