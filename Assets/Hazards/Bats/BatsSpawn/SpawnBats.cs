cd61c190f887119523a679a891c2c41bc6fbe932
César Cadillo <gkktemporal@gmail.com>
2023-07-09 16:16:05 -0600

    Add Key in BigG level 1

--- Assets/Hazards/Bats/BatsSpawn/SpawnBats.cs
+++ Assets/Hazards/Bats/BatsSpawn/SpawnBats.cs
@@ -13,22 +13,32 @@ public class SpawnBats : MonoBehaviour
 
     [SerializeField] float bullets;
 
+    //CircleCast
+    [SerializeField] bool IsSpawning;
+    [SerializeField] LayerMask player;
+    [SerializeField] float radius;
+
     
     void Start()
     {
-        
+        firePoint=GameObject.Find("firepoint").GetComponentInChildren<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        IsSpawning = Physics2D.OverlapCircle(transform.position, radius, player);
+
+        if(IsSpawning && timer >= maxTimer)
+        {
+            Spawn();
+        }
     }
 
     void Spawn()
     {
         GameObject bullet = Instantiate(bat, firePoint.position, Quaternion.identity);
-        
+        bullet.transform.position = firePoint.position;
         timer = 0;
     }
 
